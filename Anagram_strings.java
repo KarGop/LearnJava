70ef7b752a383e455b268739e9d9e6ff3f375638
KarGop <66565340+KarGop@users.noreply.github.com>
2021-03-19 23:48:21 +0530

    Add files via upload

--- Anagram_strings.java
+++ Anagram_strings.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class Anagram_strings {
 
 	public static void main(String[] args) {
-		String str1 ="listen";
-		String str2 ="silent";
+		String str1 ="silent";
+		String str2 ="listen";
 		if(str1.length()== str2.length()) {
 			char[] charArray1 = str1.toCharArray();
 			char[] charArray2 = str2.toCharArray();
 			Arrays.sort(charArray1);
 			Arrays.sort(charArray2);
-			if (charArray1.equals(charArray2)) {
+			if (Arrays.equals(charArray1,charArray2)) {
 				System.out.println(str1 + " and "+ str2 + " are Anagram");
 			}else
 					System.out.println(str1 +" and "+ str2+" are not anagram");
