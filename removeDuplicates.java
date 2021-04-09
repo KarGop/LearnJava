579450ecd1911926c8278f1741f1ebde0ac692f9
KarGop <66565340+KarGop@users.noreply.github.com>
2021-04-09 23:33:49 +0530

    Add files via upload
    PayPal India ==> ylIndi

---
+++ removeDuplicates.java
@@ -0,0 +1,33 @@
+package week3.day2;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class removeDuplicates {
+
+	public static void main(String[] args) {
+		String s = "PayPal India";
+		System.out.println("Original String : "+s);
+		char[] ch = s.toCharArray();
+		//Declare a Set as charSet for Character
+		Set<Character> charSet = new LinkedHashSet<Character>();
+		//Declare a Set as dupCharSet for duplicate Character
+		Set<Character> dupCharSet = new LinkedHashSet<Character>();
+		//Iterate character array and add it into charSet
+		for (Character eachchar : ch) {
+			//if the character is already in the charSet then, add it to the dupCharSet
+			if (!charSet.add(eachchar)) {
+				dupCharSet.add(eachchar);
+			}
+		}
+		//Check the dupCharSet elements and remove those in the charSet
+		charSet.removeAll(dupCharSet);
+		//Iterate using charSet
+		System.out.println("After removing duplicate elements :");
+		for (Character eachCh : charSet) {
+			if (eachCh!=' ') {
+				System.out.print(eachCh);
+			}
+		}		
+		}
+	}
